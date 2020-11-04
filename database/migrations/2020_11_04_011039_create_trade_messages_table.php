4db11dfdc672d8122d6a11292060d38ac7089c5e
Weje Emmanuel <wejeemmanuel34@gmail.com>
2020-11-04 01:27:55 +0000

    added trade message migration info

--- database/migrations/2020_11_04_011039_create_trade_messages_table.php
+++ database/migrations/2020_11_04_011039_create_trade_messages_table.php
@@ -15,6 +15,14 @@ class CreateTradeMessagesTable extends Migration
     {
         Schema::create('trade_messages', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id', 10);
+            $table->string('currency_from', 5);
+            $table->string('currency_to', 5);
+            $table->string('originating_country', 5);
+            $table->double('amount_sell', 8, 2);
+            $table->double('amount_buy', 8, 2);
+            $table->double('rate', 8, 6);
+            $table->datetime('time_placed');
             $table->timestamps();
         });
     }
