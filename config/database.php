98794531b8d36fea843ba8258a54e7834f3f7957
Weje Emmanuel <wejeemmanuel34@gmail.com>
2020-11-06 13:47:42 +0000

    removed cleardb code

--- config/database.php
+++ config/database.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(env("CLEARDB_DATABASE_URL"));
-$host = $url["host"] ?? null;
-$username = $url["user"] ?? null;
-$password = $url["pass"] ?? null;
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -54,9 +48,9 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database,//env('DB_DATABASE', 'forge'),
-            'username' => $username,//env('DB_USERNAME', 'forge'),
-            'password' => $password,//env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
